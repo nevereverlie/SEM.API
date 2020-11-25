1f3ee9f3486d8f5cbe610b0fef9ed9d43e508002
Hladkov Danylo <danylo.hladkov@nure.ua>
2020-11-25 22:03:01 +0200

    AuthService UserId added

--- DTOs/LoginDto.cs
+++ DTOs/LoginDto.cs
@@ -2,7 +2,6 @@ namespace Revisory_Control.API.DTOs
 {
     public class LoginDto
     {
-        public int UserId { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
